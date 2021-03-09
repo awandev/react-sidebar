cc034edeaf969197cd07ac2dd8a145872dd428f5
awandev <awal.kurniawan@uin-alauddin.ac.id>
2021-03-09 10:08:59 +0800

    level 1

---
+++ src/pages/Reports1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Reports1() {
+    return (
+        <div>
+            <h1>Reports 1</h1>
+        </div>
+    )
+}
+
+export default Reports1
---
+++ src/pages/Reports3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Reports3() {
+    return (
+        <div>
+            <h1>Reports 3</h1>
+        </div>
+    )
+}
+
+export default Reports3
